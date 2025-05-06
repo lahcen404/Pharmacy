23b4336dd7603397e16b76de8b106ae794c79ba1
lahcen404 <lahcen.maskour2003@gmail.com>
2025-05-06 12:21:39 +0100

    Create Model

--- src/main/java/com/pharmacy/pharmacy_app/model/pharmacyProduct.java
+++ src/main/java/com/pharmacy/pharmacy_app/model/pharmacyProduct.java
@@ -1,5 +1,66 @@
 package com.pharmacy.pharmacy_app.model;
 
-public class pharmacyProduct {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 
+@Entity
+public class pharmacyProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    @NotBlank(message = "name is required ")
+    private String name;
+
+    @NotBlank(message  = "Qunatity is required")
+    private int qantity;
+
+    @NotBlank(message  = "Price is required")
+    private double price;
+
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQantity() {
+        return qantity;
+    }
+
+    public void setQantity(int qantity) {
+        this.qantity = qantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
