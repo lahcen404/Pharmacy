cefdd9188819ebdafcb90bfbf9c59bf9dd3f2b5b
lahcen404 <lahcen.maskour2003@gmail.com>
2025-05-06 12:51:37 +0100

    Add Constructors Lomboks (AllArgs / NoArgs / RequiredArgs) in Model

--- src/main/java/com/pharmacy/pharmacy_app/model/pharmacyProduct.java
+++ src/main/java/com/pharmacy/pharmacy_app/model/pharmacyProduct.java
@@ -5,30 +5,35 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+
 public class pharmacyProduct {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
 
+    @NonNull
     @NotBlank(message = "Name is required ")
     private String name;
 
+    @NonNull
     @NotBlank(message  = "Quantity is required")
     private int quantity;
 
+    @NonNull
     @NotBlank(message  = "Price is required")
     private double price;
 
     private String description;
 
 
-    //getters & setters
 
 }
